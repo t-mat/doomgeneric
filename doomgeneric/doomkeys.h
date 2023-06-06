b69d31a3f09a5dd43ab5824aa70327ff2a706a46
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2023-06-07 00:07:44 +0900

    chore: Declare DOOMKEYS with enum instead of #define

--- doomgeneric/doomkeys.h
+++ doomgeneric/doomkeys.h
@@ -19,79 +19,73 @@
 #ifndef __DOOMKEYS__
 #define __DOOMKEYS__
 
-//
-// DOOM keyboard definition.
-// This is the stuff configured by Setup.Exe.
-// Most key data are simple ascii (uppercased).
-//
-#define KEY_RIGHTARROW	0xae
-#define KEY_LEFTARROW	0xac
-#define KEY_UPARROW		0xad
-#define KEY_DOWNARROW	0xaf
-#define KEY_STRAFE_L	0xa0
-#define KEY_STRAFE_R	0xa1
-#define KEY_USE			0xa2
-#define KEY_FIRE		0xa3
-#define KEY_ESCAPE		27
-#define KEY_ENTER		13
-#define KEY_TAB			9
-#define KEY_F1			(0x80+0x3b)
-#define KEY_F2			(0x80+0x3c)
-#define KEY_F3			(0x80+0x3d)
-#define KEY_F4			(0x80+0x3e)
-#define KEY_F5			(0x80+0x3f)
-#define KEY_F6			(0x80+0x40)
-#define KEY_F7			(0x80+0x41)
-#define KEY_F8			(0x80+0x42)
-#define KEY_F9			(0x80+0x43)
-#define KEY_F10			(0x80+0x44)
-#define KEY_F11			(0x80+0x57)
-#define KEY_F12			(0x80+0x58)
+enum {
+    //
+    // DOOM keyboard definition.
+    // This is the stuff configured by Setup.Exe.
+    // Most key data are simple ascii (uppercased).
+    //
+    KEY_RIGHTARROW   = 0xae,
+    KEY_LEFTARROW    = 0xac,
+    KEY_UPARROW      = 0xad,
+    KEY_DOWNARROW    = 0xaf,
+    KEY_STRAFE_L     = 0xa0,
+    KEY_STRAFE_R     = 0xa1,
+    KEY_USE          = 0xa2,
+    KEY_FIRE         = 0xa3,
+    KEY_ESCAPE       = 27,
+    KEY_ENTER        = 13,
+    KEY_TAB          = 9,
+    KEY_F1           = (0x80+0x3b),
+    KEY_F2           = (0x80+0x3c),
+    KEY_F3           = (0x80+0x3d),
+    KEY_F4           = (0x80+0x3e),
+    KEY_F5           = (0x80+0x3f),
+    KEY_F6           = (0x80+0x40),
+    KEY_F7           = (0x80+0x41),
+    KEY_F8           = (0x80+0x42),
+    KEY_F9           = (0x80+0x43),
+    KEY_F10          = (0x80+0x44),
+    KEY_F11          = (0x80+0x57),
+    KEY_F12          = (0x80+0x58),
+    KEY_BACKSPACE    = 0x7f,
+    KEY_PAUSE        = 0xff,
+    KEY_EQUALS       = 0x3d,
+    KEY_MINUS        = 0x2d,
+    KEY_RSHIFT       = (0x80+0x36),
+    KEY_RCTRL        = (0x80+0x1d),
+    KEY_RALT         = (0x80+0x38),
+    KEY_LALT         = KEY_RALT,
 
-#define KEY_BACKSPACE	0x7f
-#define KEY_PAUSE	0xff
-
-#define KEY_EQUALS	0x3d
-#define KEY_MINUS	0x2d
-
-#define KEY_RSHIFT	(0x80+0x36)
-#define KEY_RCTRL	(0x80+0x1d)
-#define KEY_RALT	(0x80+0x38)
-
-#define KEY_LALT	KEY_RALT
-
-// new keys:
-
-#define KEY_CAPSLOCK    (0x80+0x3a)
-#define KEY_NUMLOCK     (0x80+0x45)
-#define KEY_SCRLCK      (0x80+0x46)
-#define KEY_PRTSCR      (0x80+0x59)
-
-#define KEY_HOME        (0x80+0x47)
-#define KEY_END         (0x80+0x4f)
-#define KEY_PGUP        (0x80+0x49)
-#define KEY_PGDN        (0x80+0x51)
-#define KEY_INS         (0x80+0x52)
-#define KEY_DEL         (0x80+0x53)
-
-#define KEYP_0          0
-#define KEYP_1          KEY_END
-#define KEYP_2          KEY_DOWNARROW
-#define KEYP_3          KEY_PGDN
-#define KEYP_4          KEY_LEFTARROW
-#define KEYP_5          '5'
-#define KEYP_6          KEY_RIGHTARROW
-#define KEYP_7          KEY_HOME
-#define KEYP_8          KEY_UPARROW
-#define KEYP_9          KEY_PGUP
-
-#define KEYP_DIVIDE     '/'
-#define KEYP_PLUS       '+'
-#define KEYP_MINUS      '-'
-#define KEYP_MULTIPLY   '*'
-#define KEYP_PERIOD     0
-#define KEYP_EQUALS     KEY_EQUALS
-#define KEYP_ENTER      KEY_ENTER
+    // new keys:
+    KEY_CAPSLOCK    = (0x80+0x3a),
+    KEY_NUMLOCK     = (0x80+0x45),
+    KEY_SCRLCK      = (0x80+0x46),
+    KEY_PRTSCR      = (0x80+0x59),
+    KEY_HOME        = (0x80+0x47),
+    KEY_END         = (0x80+0x4f),
+    KEY_PGUP        = (0x80+0x49),
+    KEY_PGDN        = (0x80+0x51),
+    KEY_INS         = (0x80+0x52),
+    KEY_DEL         = (0x80+0x53),
+    KEYP_0          = 0,
+    KEYP_1          = KEY_END,
+    KEYP_2          = KEY_DOWNARROW,
+    KEYP_3          = KEY_PGDN,
+    KEYP_4          = KEY_LEFTARROW,
+    KEYP_5          = '5',
+    KEYP_6          = KEY_RIGHTARROW,
+    KEYP_7          = KEY_HOME,
+    KEYP_8          = KEY_UPARROW,
+    KEYP_9          = KEY_PGUP,
+    KEYP_DIVIDE     = '/',
+    KEYP_PLUS       = '+',
+    KEYP_MINUS      = '-',
+    KEYP_MULTIPLY   = '*',
+    KEYP_PERIOD     = 0,
+    KEYP_EQUALS     = KEY_EQUALS,
+    KEYP_ENTER      = KEY_ENTER,
+};
 
 #endif          // __DOOMKEYS__
 
